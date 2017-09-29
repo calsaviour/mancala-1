cc01171a65549d52549245c9d7f86c89ca2c28cb
matt lubel <matt_lubel@hotmail.com>
2017-09-29 16:39:08 +0100

    some rubbish styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,48 @@
 import React from 'react'
 
-const Board = ({ board, clickHandler }) => (
-  <div>
-  	{board.map((hole, i) => (
-      <button key={i} onClick={() => clickHandler(i)}>
-        {hole}
-      </button>
-    ))}
-  </div>
-)
+const Board = ({ board, clickHandler }) => {
+
+  const boardWithIndexes = board.map((n, i) => ({ count: n, index: i }))
+
+  const player1Holes = boardWithIndexes.slice(0, 6)
+  const player2Holes = boardWithIndexes.slice(7, 13)
+
+  const player1Home = board[6]
+  const player2Home = board[13]
+
+  return (
+    <div className="board">
+
+      <div className="home">
+        { player2Home }
+      </div>
+
+      <div className="holes-section">
+
+        <div className="holes-container">
+        	{player2Holes.reverse().map((hole, i) => (
+            <div className="hole" key={i} onClick={() => clickHandler(hole.index)}>
+              { hole.count }
+            </div>
+          ))}
+        </div>
+
+        <div className="holes-container">
+          {player1Holes.map((hole, i) => (
+            <div className="hole" key={i} onClick={() => clickHandler(hole.index)}>
+              { hole.count }
+            </div>
+          ))}
+        </div>
+
+      </div>
+
+      <div className="home">
+        { player1Home }
+      </div>
+
+    </div>
+  )
+}
 
 export default Board
